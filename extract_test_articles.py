89e92e7d51f10b3576ba4552b6e843256b6f4a5b
Aadil <33660637+aadilk97@users.noreply.github.com>
2019-01-26 19:19:42 +0530

    Dump articles for testing

---
+++ extract_test_articles.py
@@ -0,0 +1,137 @@
+import pickle
+import numpy as np
+import os
+import json
+import requests
+import sys
+import nltk
+import time
+
+
+from os.path import join, isfile
+from goose3 import Goose, Configuration
+from nltk import word_tokenize
+
+from web_res2 import compute_overlap
+from vectorize_biaslex import find_vector
+
+
+
+path = '/Users/aadil/fake_news_detection/Snopes'
+dump_path = '/Users/aadil/fake_news_detection/test'
+
+filepaths = [join(path, f) for f in os.listdir(path) if isfile(join(path, f))]
+filepaths.sort()
+
+
+def extract_articles(i):
+	articles = []
+	g = Goose()
+
+
+	with open(filepaths[i]) as f:
+		urls = []
+		data = json.load(f)
+		name = data['Claim_ID']
+		query = data['Claim']
+
+
+		for item in data['Google Results'][0]['results']:
+			if 'snopes' not in item['link'] and 'pdf' not in item['link']:
+				urls.append(item['link'])
+
+		if data['Credibility'] == 'false':
+			cred = '0'
+
+		else:
+			cred = '1'
+
+	print ('At claim .... ', name)
+	count = 0
+	checkpoint = time.time()
+	timeout = 5
+
+	for url in urls:
+		try:
+			## Extracting the articles using goose and extracting the text body and checking overlap with the claim
+			article = g.extract(url=url)							
+			article = article.cleaned_text
+			score, res = compute_overlap(query, article)
+
+
+			## Checking the limit of maximum 5 articles per claim
+			if count > 10:
+				break
+
+			## Checking if the overlap is greater than a threshold value
+			if res == True:
+				articles.append(article)
+				count += 1
+				checkpoint = time.time()
+				filepath = os.path.join(dump_path, name, (str(count) + '.txt'))
+				os.makedirs(os.path.dirname(filepath), exist_ok=True)
+				f = open(filepath, 'w')
+				f.write(article)
+				f.close()
+
+			# if time.time() > checkpoint + timeout:
+			# 	print ('Timed-out ....', name)
+			# 	break
+
+		## Checking for connection error of requests
+		except requests.exceptions.ConnectionError as e:
+			# print ('Some error at file =', name)
+			continue
+
+		except:
+			# print (sys.exc_info()[0])
+			continue
+
+	g.close()
+	return articles, cred, name
+
+
+for i in range(4500, 4600):
+	if i % 20 == 0:
+		print ('At step = ', i)
+
+	articles, label, name = np.array(extract_articles(i))
+
+	if len(articles) > 0:
+		print ('Recieved something')
+		print ()
+		label_path = os.path.join(dump_path, name, 'label.txt')
+		f = open(label_path, 'w')
+		f.write(label)
+		f.close()
+		
+
+
+
+# with open('y_test(4300).pkl', 'rb'):
+# 	y_old = pickle.load(f)
+
+# y_new = np.concatenate([y_old, y_test])
+
+# print ('Dumping y_test')
+# with open('y_test.pkl', 'wb') as f:
+# 	pickle.dump(y_new, f)
+
+# true_acc = []
+# false_acc = []
+# for train_index, test_index in kf.split(X):
+# 	X_train, y_train = X[train_index], y[train_index]
+# 	X_test, y_test = X[test_index], y[test_index]
+
+# 	clf.fit(X_train, y_train)
+# 	pred = clf.predict(X_test)
+
+# 	tn, fp, fn, tp = confusion_matrix(y_test, pred).ravel()
+# 	true_acc.append(tp / (tp + fn))
+# 	false_acc.append(tn / (fp + tn))
+
+# true_acc = np.array(true_acc)
+# false_acc = np.array(false_acc)
+
+# print (clf.predict_proba(X_train))
+
