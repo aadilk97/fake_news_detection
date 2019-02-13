efc707ce658a627ac946ff5e8ab5fa55b0be45cd
Aadil <33660637+aadilk97@users.noreply.github.com>
2019-02-13 10:53:58 +0530

    Update extract_test_articles.py

--- extract_test_articles.py
+++ extract_test_articles.py
@@ -7,21 +7,59 @@ import sys
 import nltk
 import time
 
-
 from os.path import join, isfile
 from goose3 import Goose, Configuration
 from nltk import word_tokenize
-
-from web_res2 import compute_overlap
-from vectorize_biaslex import find_vector
+from nltk.corpus import stopwords
+from googlesearch import search 
+from urllib.parse import urlparse
 
 
 
 path = '/Users/aadil/fake_news_detection/Snopes'
-dump_path = '/Users/aadil/fake_news_detection/test'
+dump_path = '/Users/aadil/fake_news_detection/test_30hits'
+stop_words = set(stopwords.words('english'))
+
 
 filepaths = [join(path, f) for f in os.listdir(path) if isfile(join(path, f))]
 filepaths.sort()
+
+
+def is_relevant(claim, article):
+	sentences = nltk.sent_tokenize(article)
+	sentences = [sent for sent in sentences if len(word_tokenize(sent)) > 3]
+
+	i = 0
+	snippets = []
+	while i < (len(sentences)-3):
+		snippet = ''
+		for sent in sentences[i:i+3]:
+			snippet += sent
+
+		tk_claim = word_tokenize(claim)
+		tk_snippet = word_tokenize(snippet)
+		tk_clean_snippet = [i for i in tk_snippet if i not in stop_words]
+
+		c1 = c2 = 0
+		for t in tk_claim:
+			if t in tk_clean_snippet:
+				c1 += 1
+
+		bigrm1 = list(nltk.bigrams(tk_claim))
+		bigrm2 = list(nltk.bigrams(tk_snippet))
+
+		for b in bigrm1:
+			if b in bigrm2:
+				c2 += 1
+
+		score = c1 + c2
+		if score >= 0.2 * (len(tk_claim) + len(bigrm1)):
+			return True
+
+		else:
+			i += 1
+
+	return False
 
 
 def extract_articles(i):
@@ -38,6 +76,7 @@ def extract_articles(i):
 
 		for item in data['Google Results'][0]['results']:
 			if 'snopes' not in item['link'] and 'pdf' not in item['link']:
+				item['link'] = item['link'].replace('https', 'http')
 				urls.append(item['link'])
 
 		if data['Credibility'] == 'false':
@@ -46,26 +85,38 @@ def extract_articles(i):
 		else:
 			cred = '1'
 
+		urls_google = []
+		try:
+			for url in search(query, stop=30):
+				if 'snopes' not in url:
+					urls_google.append(url)
+		except:
+			print ('Some error')
+			
+
+		for url in urls_google:
+			url = url.replace('https', 'http')
+			if url not in urls:
+				urls.append(url)
+
 	print ('At claim .... ', name)
 	count = 0
 	checkpoint = time.time()
 	timeout = 5
-
+	extracted_articles_domains = []
 	for url in urls:
 		try:
 			## Extracting the articles using goose and extracting the text body and checking overlap with the claim
 			article = g.extract(url=url)							
 			article = article.cleaned_text
-			score, res = compute_overlap(query, article)
-
-
-			## Checking the limit of maximum 5 articles per claim
-			if count > 10:
-				break
+			res = is_relevant(query, article)
 
 			## Checking if the overlap is greater than a threshold value
 			if res == True:
 				articles.append(article)
+				parsed_uri = urlparse(url)
+				domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+				extracted_articles_domains.append(domain)
 				count += 1
 				checkpoint = time.time()
 				filepath = os.path.join(dump_path, name, (str(count) + '.txt'))
@@ -88,14 +139,14 @@ def extract_articles(i):
 			continue
 
 	g.close()
-	return articles, cred, name
+	return articles, cred, name, query, extracted_articles_domains
 
 
-for i in range(4500, 4600):
+for i in range(4080, 4200):
 	if i % 20 == 0:
 		print ('At step = ', i)
 
-	articles, label, name = np.array(extract_articles(i))
+	articles, label, name, claim, domains = np.array(extract_articles(i))
 
 	if len(articles) > 0:
 		print ('Recieved something')
@@ -104,8 +155,20 @@ for i in range(4500, 4600):
 		f = open(label_path, 'w')
 		f.write(label)
 		f.close()
-		
 
+		claim_path = os.path.join(dump_path, name, 'claim.txt')
+		f = open(claim_path, 'w')
+		f.write(claim)
+		f.close()
+
+		domain_text = ''
+		for domain in domains:
+			domain_text += domain + '\n'
+
+		domains_path = os.path.join(dump_path, name, 'domains.txt')
+		f = open(domains_path, 'w')
+		f.write(domain_text)
+		f.close()
 
 
 # with open('y_test(4300).pkl', 'rb'):
