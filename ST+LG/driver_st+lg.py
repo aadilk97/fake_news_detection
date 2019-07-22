fb2a1c5c8e0a37d12ca2d3135d248926175bbca4
Aadil <33660637+aadilk97@users.noreply.github.com>
2019-07-22 20:23:18 +0530

    Update driver_st+lg.py

--- ST+LG/driver_st+lg.py
+++ ST+LG/driver_st+lg.py
@@ -27,14 +27,14 @@ for i in range(len(lexpaths)):
 
 		lexicons.insert(i, word_list)
 
-
+## Extracting the articles using Goose 
 def extract_articles(claim):
 	print ('Extracting the articles ...')
 	g = Goose()
 	articles = []
 	for url in search(claim, stop=10):
 		if 'snopes' not in url:
-			try: 
+			try:
 				article = g.extract(url=url)
 				articles.append(article.cleaned_text)
 			except:
@@ -45,12 +45,16 @@ def extract_articles(claim):
 def extract_snippets(claim, article):
 	text = ''
 	sentences = nltk.sent_tokenize(article)
+
+	## Removing sentences with less than 4 words
 	sentences = [sent for sent in sentences if len(word_tokenize(sent)) > 3]
 
 	i = 0
 	snippets = []
 	snippets_cs = []
 	overlap_score = []
+
+	## Generating a set of snippets of 3 consecutive sentences and tokenizing it
 	while i < (len(sentences)-3):
 		snippet = ''
 		for sent in sentences[i:i+3]:
@@ -60,6 +64,7 @@ def extract_snippets(claim, article):
 		tk_snippet = word_tokenize(snippet)
 		tk_clean_snippet = [i for i in tk_snippet if i not in stop_words]
 
+		## Calculating the unigram and bigram overlap scores
 		c1 = c2 = 0
 		for t in tk_claim:
 			if t in tk_clean_snippet:
@@ -73,6 +78,8 @@ def extract_snippets(claim, article):
 				c2 += 1
 
 		score = c1 + c2
+
+		## Removing the snippets with less than 20% overlap
 		if score >= 0.2 * (len(tk_claim) + len(bigrm1)):
 			snippet = ' '.join([w for w in word_tokenize(snippet)])
 			snippets_cs.append(snippet)
@@ -88,6 +95,7 @@ def extract_snippets(claim, article):
 
 	return snippets, snippets_cs, overlap_score
 
+## Vectorizing each snippet based on lexicons
 def find_vector(corpus):
 	X = []
 	for text in corpus:
@@ -103,6 +111,7 @@ def find_vector(corpus):
 
 	return X
 
+## Used for finding snippets with subjective words as it can be a good indicator towards evidence
 def find_subjectvity(text):
 	tokens = word_tokenize(text)
 	c = 0
@@ -116,6 +125,8 @@ claim = input()
 articles = extract_articles(claim)
 
 
+## Loading all the trained classifiers
+## clf_st = stance classifier, clf_st_lg = Stance and linguistic features classifier, vectorizer = tf-idf vectorizer
 clf_st = pickle.load(open('stance_classifier.pkl', 'rb'))
 clf_st_lg = pickle.load(open('classifier_st-lg.pkl', 'rb'))
 vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
@@ -131,6 +142,7 @@ for i in range(len(articles)):
 	per_snippet_score = []
 	per_snippet_stance = []
 	if len(snippets) > 0:
+		## Vectorzing each snipet using the tf-idf vectorizer and obtaining the stance using the stance classifier
 		vec = vectorizer.transform(snippets).toarray()
 		per_snippet_stance.append(clf_st.predict(vec))
 		per_snippet_score.append(clf_st.predict_proba(vec))
@@ -139,22 +151,15 @@ for i in range(len(articles)):
 		overlap_score = np.array(overlap_score)
 
 
-		# claims = claims[(-overlap_score).argsort()[:claims.shape[0]]]
-		# per_snippet_stance = per_snippet_stance[(-overlap_score).argsort()[:per_snippet_stance.shape[0]]]
-
 		evidences.append((snippets_cs[np.argmax(overlap_score)], per_snippet_stance[np.argmax(overlap_score)]))
 		
+		## Obtaining the linguistic features and giving it to the stance and linguitic classifier(clf_st_lg)
 		f_l = np.array(find_vector(snippets))
 		F = np.concatenate((per_snippet_score, f_l), axis=1)
 		article_prediction = clf_st_lg.predict_proba(F)
 		per_article_pred.append((np.average(article_prediction, axis=0)))
 
 if len(per_article_pred) > 0:
-	## Giving the most common class as the overll prediction
-	# per_article_pred = np.argmax(per_article_pred, axis=1) 
-	# count = np.bincount(per_article_pred)
-	# pred = np.argmax(count)
-
 	## Calculating the sum of probabilities of each article for overall prediction
 	pred = np.argmax(np.average(per_article_pred, axis=0))
 	print ('Prediction = ', pred)
@@ -165,7 +170,6 @@ subjectivity_score = np.array([find_subjectvity(evidence[0]) for evidence in evi
 order = (-subjectivity_score).argsort()[:subjectivity_score.shape[0]]
 
 if pred == 0:
-	# evidences = evidences[(-subjectivity_score).argsort()[:subjectivity_score.shape[0]]]
 	evidences = [evidences[i] for i in order]
 
 ## Finding the evidence
